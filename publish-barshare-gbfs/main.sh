0117c07ffb63f906a4d501ce43f400edb46a6ef1
Volker Kerkhoff <volker_kerkhoff@epam.com>
2024-01-25 12:25:00 +0100

    URBIBBNA-42
    Falscher Build-Context

--- publish-barshare-gbfs/main.sh
+++ publish-barshare-gbfs/main.sh
@@ -25,7 +25,7 @@ dir="$(mktemp -d -t barshare-gbfs.XXXXXX)"
 
 set -x
 
-mcli cp -q index.html barshare-logo.png bbnavi/barshare/
+mc cp -q index.html barshare-logo.png bbnavi/barshare/
 
 while true; do
 	# We sleep first so that, if the GBFS generation fails contantly, we don't DOS the Moqo API.
@@ -39,5 +39,5 @@ while true; do
 
 	tree -sh "$dir"
 
-	mcli cp -q -r $dir/* bbnavi/barshare/
+	mc cp -q -r $dir/* bbnavi/barshare/
 done
